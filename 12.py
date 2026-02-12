e8e9ac2e44ec43c499f47828feacd7129c114c55
sreeruthin <162143379+sreeruthin@users.noreply.github.com>
2026-02-12 19:55:59 +0530

    output

---
+++ 12.py
@@ -0,0 +1,8 @@
+import cv2
+import numpy as np
+
+img = cv2.imread("input.jpg", 0)
+edges = cv2.Canny(img, 100, 200)
+cv2.imshow("Canny Edge Detection", edges)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
