40576cef75eae482bc27cc722725fbdc2f542c83
sreeruthin <162143379+sreeruthin@users.noreply.github.com>
2026-02-12 20:44:28 +0530

    output

---
+++ 27.py
@@ -0,0 +1,9 @@
+import cv2
+import numpy as np
+
+img = cv2.imread("1a.png", 0)
+kernel = np.ones((5, 5), np.uint8)
+closing = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
+cv2.imshow("Closing", closing)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
