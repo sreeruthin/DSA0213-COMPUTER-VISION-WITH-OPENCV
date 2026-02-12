88880c6d05080f87b878760df0d2a51371daf5eb
sreeruthin <162143379+sreeruthin@users.noreply.github.com>
2026-02-12 20:33:26 +0530

    output

---
+++ 23.py
@@ -0,0 +1,11 @@
+import cv2
+import numpy as np
+
+img = cv2.imread("1a.png", 0)
+kernel = np.array([[-1, -1, -1],
+                   [-1,  8, -1],
+                   [-1, -1, -1]])
+boundary = cv2.filter2D(img, -1, kernel)
+cv2.imshow("Boundary Image", boundary)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
